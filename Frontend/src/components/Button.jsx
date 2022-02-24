e864b9c8a819b0b502277989ea06222bb2a0b7e0
kevinlobos42 <51803493+kevinlobos42@users.noreply.github.com>
2022-02-23 21:06:24 -0500

    Delete Frontend directory

--- Frontend/src/components/Button.jsx
+++
@@ -1,9 +0,0 @@
-import '../css/button.css'
-
-function Button({color, text}) {
-  return (
-    <button className={"btn btn-" + (color==="white"?'white':'blue')}>{text}</button>
-  )
-}
-
-export default Button
